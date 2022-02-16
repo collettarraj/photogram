c67ccf6078b1643c6e9f539c18899c7d34bae8b2
anonymous anonymous <collett.arraj@kellogg.northwestern.edu>
2022-02-16 13:46:12 +0000

    Only usersbest user can modify usersbest

--- app/controllers/usersbests_controller.rb
+++ app/controllers/usersbests_controller.rb
@@ -1,4 +1,6 @@
 class UsersbestsController < ApplicationController
+  before_action :current_user_must_be_usersbest_users, only: [:edit, :update, :destroy] 
+
   before_action :set_usersbest, only: [:show, :edit, :update, :destroy]
 
   # GET /usersbests
@@ -57,6 +59,14 @@ class UsersbestsController < ApplicationController
 
 
   private
+
+  def current_user_must_be_usersbest_users
+    set_usersbest
+    unless current_user == @usersbest.users
+      redirect_back fallback_location: root_path, alert: "You are not authorized for that."
+    end
+  end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_usersbest
       @usersbest = Usersbest.find(params[:id])
