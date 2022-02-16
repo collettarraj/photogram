1f92fe156f3c6942c7bfb4db4c56570e923f7b56
anonymous anonymous <collett.arraj@kellogg.northwestern.edu>
2022-02-16 13:45:57 +0000

    Whitelist columns in Usersbest for ActiveAdmin

--- app/admin/usersbests.rb
+++ app/admin/usersbests.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Usersbest do
 
+ permit_params :dishes_id, :venue_id, :users_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
